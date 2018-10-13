527184d8510a52604c23bf06ca61785e45768142
“relner” <vladimir@relner.com>
2018-10-13 13:01:30 +0300

    add commit

--- src/app/pipes/sound-cloud.pipe.spec.ts
+++ src/app/pipes/sound-cloud.pipe.spec.ts
@@ -2,7 +2,7 @@ import { SoundCloudPipe } from './sound-cloud.pipe';
 
 describe('SoundCloudPipe', () => {
   it('create an instance', () => {
-    const pipe = new SoundCloudPipe();
+    const pipe = new SoundCloudPipe(null);
     expect(pipe).toBeTruthy();
   });
 });
